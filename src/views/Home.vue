9eefb31deef71387d98aa6fb5ded72875d63a781
Franklin Embate <ferox.dragon@gmail.com>
2019-02-28 11:01:42 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+	<video src="" ref="localVideo"></video>
+	<video src="" ref="remoteVideo"></video>
+	<br>
+	<button @click="start">Start</button>
   </div>
 </template>
 
@@ -14,5 +16,54 @@ import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
     HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+	localVideo: any = null
+	remoteVideo: any = null
+	localStream: any = null
+	socket: any  = null
+
+	connected: boolean = false
+
+	config: any = { iceServers: [{url: "stun:stun.l.google.com:19302"}]}
+	pc: any = new RTCPeerConnection(this.config)
+	pc2 = new RTCPeerConnection(this.config);
+
+	mediaConstraints: any = {
+	  mandatory: {
+	    OfferToReceiveAudio:true, 
+	    OfferToReceiveVideo:true
+	  }
+	}
+
+	onmessage(message: any) {
+		var msg = JSON.parse(message.data);
+	}
+
+	mounted() {
+		this.socket = new WebSocket('ws://192.168.0.103:9090')
+
+		this.localVideo = this.$refs.localVideo
+		this.remoteVideo = this.$refs.remoteVideo
+
+		this.localVideo.addEventListener('loadedmetadata', () => {
+			this.localVideo.play()
+		});
+
+		this.socket.onmessage = this.onmessage
+
+		navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then((stream: any) => {
+			this.localVideo.srcObject = stream
+			this.pc.addStream(stream)
+
+			this.pc.ontrack = (e: any) => this.remoteVideo.srcObject = e.streams[0]
+		}).catch(function(err) {
+			console.log(err.name + ": " + err.message); 
+		})
+	}
+
+	start() {
+		// this.pc.createOffer(this.mediaConstraints).then(this.createOffer);
+	}
+
+}
 </script>
